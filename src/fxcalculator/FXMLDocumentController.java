6e69b0b6e885ada73a27bf2ff8ade4886a2421a3
n231055a <n231055a@milais-mba-155.classroom.milais>
2014-11-07 11:46:02 +0900

    add command

--- src/fxcalculator/FXMLDocumentController.java
+++ src/fxcalculator/FXMLDocumentController.java
@@ -10,7 +10,8 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 /**
  *
@@ -18,13 +19,19 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
     
+    private int register=0;
+    private boolean opeAdd=false;
     @FXML
-    private Label label;
+    private TextField accumulater;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void KeyAction(ActionEvent event) {
+        Button b = (Button)event.getSource();
+        if(opeAdd == true){
+            accumulater.setText("0");
+            opeAdd = false;
+        }
+        
     }
     
     @Override
